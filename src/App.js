090a3ed3edccbcba9a4b9272012c62feb7c228e5
Alejandro Hermosilla <51329486+aleunfor@users.noreply.github.com>
2022-05-11 13:36:24 -0400

    Update Canonical

--- src/App.js
+++ src/App.js
@@ -5,28 +5,34 @@ import { Route, Switch } from 'wouter';
 import Detail from './Pages/Detail';
 
 import { GifsContextProvider } from './context/GifsContext';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
-    <div className="container mt-3">
-      <a href="/"><h1 className='text-center mb-3'>UnforGif</h1></a>
-      <GifsContextProvider>
-        <Switch>
-          <Route
-            component={Home}
-            path="/" />
-          <Route
-            component={SearchResults}
-            path="/search/:keyword" />
-          <Route
-            component={Detail}
-            path="/gif/:id" />
-          <Route
-            component={() => <h1>Error 404 :(</h1>}
-            path="/404" />
-        </Switch>
-      </GifsContextProvider>
-    </div>
+    <>
+      <Helmet>
+        <link rel="canonical" href="https://unforgif.vercel.app/" />
+      </Helmet>
+      <div className="container mt-3">
+        <a href="/"><h1 className='text-center mb-3'>UnforGif</h1></a>
+        <GifsContextProvider>
+          <Switch>
+            <Route
+              component={Home}
+              path="/" />
+            <Route
+              component={SearchResults}
+              path="/search/:keyword" />
+            <Route
+              component={Detail}
+              path="/gif/:id" />
+            <Route
+              component={() => <h1>Error 404 :(</h1>}
+              path="/404" />
+          </Switch>
+        </GifsContextProvider>
+      </div>
+    </>
   );
 }
 
