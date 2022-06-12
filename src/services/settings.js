56a8801bff0e0968e3e3bf0967246aa2ae23e0b0
Alejandro Hermosilla <51329486+aleunfor@users.noreply.github.com>
2022-06-11 22:23:38 -0400

    Update settings.js

--- src/services/settings.js
+++ src/services/settings.js
@@ -2,4 +2,4 @@ export const API_KEY = 'TYzrzl459LN5ACAr15atAFgCKBdvaDxg';
 
 export const API_URL = 'https://api.giphy.com/v1';
 
-export const ENDPOINT = 'http://fixie:4cl1q5VIxPRwnFl@velodrome.usefixie.com:80'
+export const ENDPOINT = 'https://back-end-unforgif.herokuapp.com'
