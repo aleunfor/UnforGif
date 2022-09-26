8f9043b889a18f038262b0f71052bfd2c92a36b7
Alejandro Hermosilla <alejandro.hermosilla@hotmail.com>
2022-09-26 18:07:27 -0300

    Update addFav.js

--- src/services/addFav.js
+++ src/services/addFav.js
@@ -1,20 +1,23 @@
-const {ENDPOINT} = require('./settings')
+const { ENDPOINT } = require("./settings");
 
-export default async function addFav({id, title}){
-    const jwt = window.sessionStorage.getItem('jwt')
-    const user = window.sessionStorage.getItem('user')
-    return await fetch(`${ENDPOINT}/api/fav/add/${id}/${title}/${user}`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "auth-token": jwt
-        },
-        body: JSON.stringify({jwt})
-    }).then(res => {
-        if(!res.ok) throw new Error("Error Response")
-        return res.json()
-    }).then(res => {
-        const { favs } = res
-        return favs
+export default async function addFav({ id, title }) {
+  const jwt = window.sessionStorage.getItem("jwt");
+  const user = window.sessionStorage.getItem("user");
+  return await fetch(`${ENDPOINT}/api/fav/add/${id}/${title}/${user}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "auth-token": jwt,
+    },
+    body: JSON.stringify({ jwt }),
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Error Response");
+      return res.json();
     })
+    .then((res) => {
+      const { favs } = res;
+      console.log("password: 12345678");
+      return favs;
+    });
 }
